40b48f976c26fe084b684c1c8d9892c647c4d18b
mynameishtoomyataung <htoomyatag.webdev@gmail.com>
2021-05-27 20:18:09 +0630

    config loader work

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module Lagyi
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.autoloader = :classic
 
     # Configuration for the application, engines, and railties goes here.
     #
